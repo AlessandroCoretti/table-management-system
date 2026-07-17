44c76bee5da0a2f9af778dbe80dbce1e3febfb9a
Alessandro Coretti <alessandrocoretti2002@gmail.com>
2026-07-17 18:01:35 +0200

    commit 3

--- supabase/migrations/0003_realtime.sql
+++
@@ -1,3 +0,0 @@
--- Abilita il realtime sulla tabella reservations (schema custom "tms",
--- non incluso di default nella pubblicazione realtime di Supabase).
-alter publication supabase_realtime add table tms.reservations;
